3f268c662e6f02e588c32c0c3b3b0e3757100e78
Adam Tse <mr.adam.tse@gmail.com>
2019-09-19 19:54:07 +0800

    recipe list component setup

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -8,10 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  
+
   constructor() { }
 
   ngOnInit() {
+    this.recipes.push(new Recipe("Quick Steak", "Cook a amazing Steak in 5 mins", "https://media1.s-nbcnews.com/j/newscms/2018_07/1318715/grilled-steak-today-tease-180216_89508b219dd455b4d43311782841f938.today-inline-large.jpg"))
+    this.recipes.push(new Recipe("Steak and Broccoli", "steak and brocolli", "https://cms.splendidtable.org/sites/default/files/styles/w2000/public/Jalapeno_Honey_Steak_JPG-LEDE.jpg?itok=qDtX2pmz"))
   }
 
 }
