179ec4b185682b5b5026b42e2f235efa5e90da50
Adam Tse <mr.adam.tse@gmail.com>
2019-09-19 20:59:05 +0800

    recipe to detail, simple navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  optionLoaded: string = "recipes"
+
+  onNavigation(option){
+    this.optionLoaded = option
+  }
 }
