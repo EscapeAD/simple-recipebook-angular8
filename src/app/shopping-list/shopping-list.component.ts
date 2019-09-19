5c8de5f52f268c0740ba8a9fd68d8f3776367448
Adam Tse <mr.adam.tse@gmail.com>
2019-09-19 20:35:07 +0800

    Shopping edit component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = []
   constructor() { }
 
   ngOnInit() {
+    this.ingredients.push(new Ingredient("New York Sorlin", 200, "g"))
+    this.ingredients.push(new Ingredient("Butter", 50, "g"))
   }
 
 }
